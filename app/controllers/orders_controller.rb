62383306b96a025278a3425271bd9551cfc97261
mstop4 <mstop4@gmail.com>
2017-08-03 16:45:18 +0000

    can only display most recent order once

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @cart = cart
+    empty_cart!
   end
 
   def create
@@ -9,7 +11,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
-      empty_cart!
+      #empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, error: order.errors.full_messages.first
