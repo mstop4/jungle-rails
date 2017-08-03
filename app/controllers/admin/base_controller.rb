67a6483f268019d4e5f6a7f4a12888b11e655309
mstop4 <mstop4@gmail.com>
2017-08-03 23:34:17 +0000

    BaseController puzzle solved

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,5 @@
+class Admin::BaseController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_ID'], password: ENV['ADMIN_PASSWORD']
+
+end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,4 @@
-class Admin::DashboardController < ApplicationController
-
-  http_basic_authenticate_with name: ENV['ADMIN_ID'], password: ENV['ADMIN_PASSWORD']
+class Admin::DashboardController < Admin::BaseController
 
   def show
   end
