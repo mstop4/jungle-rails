2a1738da8e92607b49a49a1b797b09fae528aaf5
mstop4 <mstop4@gmail.com>
2017-08-03 13:22:30 +0000

    add user MVC

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  #resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
