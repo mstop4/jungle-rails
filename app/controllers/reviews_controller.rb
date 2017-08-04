79f1ce8baa5b9d42d4496947b053ee5a6cb51bf1
mstop4 <mstop4@gmail.com>
2017-08-04 20:27:16 +0000

    more improvements to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,6 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.product_id = params[:product_id]
-    byebug
     @review.user_id = current_user.id
 
     if @review.save
