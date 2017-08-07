7d35bea8ec068d22019637d0af5360c077ecd493
mstop4 <mstop4@gmail.com>
2017-08-07 15:18:00 +0000

    fixed ratings in view categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,12 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+
+    @ratings = {}
+
+    @products.each do |product|
+      @ratings[product.id] = { average: product.reviews.average(:rating) , number: product.reviews.length }
+    end
   end
 
 end
