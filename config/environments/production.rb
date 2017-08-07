3d7cd867f8e1696d904467c25de0d634fca33219
mstop4 <mstop4@gmail.com>
2017-08-07 21:40:08 +0000

    testing mail 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    :user_name => ENV ['SENDGRID_USERNAME'],
-    :password => ENV ['SENDGRID_PASSWORD'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'jungle.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
