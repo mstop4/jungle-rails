d22e850dee1b9af7d7249167332d624922553126
mstop4 <mstop4@gmail.com>
2017-08-07 21:37:55 +0000

    testing mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV ['SENDGRID_USERNAME'],
+    :password => ENV ['SENDGRID_PASSWORD'],
+    :domain => 'jungle.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   config.action_mailer.raise_delivery_errors = true
 end
