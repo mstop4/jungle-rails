04d3fa779470ad9ac2f198dc400742fb716259b2
mstop4 <mstop4@gmail.com>
2017-08-08 17:04:52 +0000

    testing user login

---
+++ spec/features/user_logins_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+
+  before :each do
+    @user = User.create!({
+      first_name: "Fritz",
+      last_name: "Schnackenpfefferhausen",
+      email: "mein@bratwurst.com",
+      password: "fritz",
+      password_confirmation: "fritz"
+    })
+  end
+
+  scenario "They log in" do
+    # ACT
+    visit root_path
+    find_link(href: "/login").click
+
+    fill_in "email", with: "mein@bratwurst.com"
+    fill_in "password", with: "fritz"
+    find_button("Submit").click
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_text "Hello, Fritz!"
+  end
+
+end
