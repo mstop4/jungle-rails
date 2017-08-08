0a77d6ef0094f86f994002edd0f3573cc873a60f
mstop4 <mstop4@gmail.com>
2017-08-08 15:50:20 +0000

    product details spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor looks at a product", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the details of a product" do
+    # ACT
+    visit root_path
+    find_link("Details »", href: "/products/5").click
+
+    # DEBUG
+    save_screenshot
+    puts page.html
+
+    # VERIFY
+    expect(page).to have_css '.products-show'
+  end
+end
